df99aa2a21025b9663a1723c57e4718c1cb136c7
Mohit Jangra <mohitjangra6017@gmail.com>
2021-10-13 09:37:47 +0530

    add file

--- FIle/SignUp.py
+++ FIle/SignUp.py
@@ -1,6 +1,8 @@
+import re
+
+import self as self
 from selenium import webdriver
 from selenium.webdriver.chrome.webdriver import WebDriver
-from selenium.webdriver.common.keys import Keys
 
 # Page Convert Hindi to English
 custom_options = webdriver.ChromeOptions()
@@ -13,17 +15,37 @@ driver: WebDriver = webdriver.Chrome(executable_path="../driver/chromedriver.exe
 
 driver.maximize_window()
 
-driver.get("https://www.facebook.com/")
+driver.get("http://demowebshop.tricentis.com/")
 
 print(driver.title)
 # print(driver.page_source)
 print(driver.current_url)
 
-
-if(driver.getPageSource().contains("Facebook")):{
-print("Facebook helps you connect and share with people.")
-}
+# verify facebook logo is visible
+logo = driver.find_element_by_xpath("//img[contains(@src,'logo.png')]")
+if 'logo' in logo.get_attribute('src'):
+    print("Web shop logo is visible on UI")
 else:
-    {
-       print("Text is absent")
-    }
+    print('Web Shop Logo Not Visible On UI')
+
+# verify create account button and click on
+
+regiser = driver.find_element_by_xpath("//a[contains(@href,'/register')]")
+if 'ico-register' in regiser.get_attribute('class'):
+    print("Register Button Is Visible")
+    regiser.click()
+    print("Click Success!")
+else:
+    print("Register Button Is Not Visible")
+
+# verify signup details
+
+# title = driver.find_element_by_xpath("//div[@class='page-title']//h1[text()='Register']")
+# text = 'Register'
+# if text in h1:
+#     print("Title is Register")
+
+content = driver.page_source
+abc = content.find('Register')
+print("text is present in the webpage", abc)
+
